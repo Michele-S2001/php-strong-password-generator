f28e4b09cf4d31bb5224fcf230db77a2257e8baf
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-23 17:42:20 +0100

    ADD > Styling with boostrap

--- index.php
+++ index.php
@@ -11,20 +11,29 @@ $int_pass_length = intval($pass_length);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Strong Password Generator</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
-<body>
-  
-  <h3>Genera una password random</h3>
-  <!-- form -->
-  <form action="" method="get">
-    <input name="pass_length" type="text" placeholder="Inserire lunghezza password">
-    <button type="submit">Genera</button>
-  </form>
-
-  <?php if($int_pass_length > 0) { ?>
-    <p><?= getRandomPassword($pass_length) ?></p>
-  <?php } else { ?>
-    <p>Inserire numero maggiore di 0 !</p>
-  <?php } ?>
+<body class="bg-black pt-4" style="color: white">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-auto text-center">
+        <h3 class="mb-4">Genera una password random</h3>
+        <!-- form -->
+        <form class="mb-2 d-flex gap-2" action="" method="get">
+          <input class="form-control" name="pass_length" type="text" placeholder="Inserire lunghezza password">
+          <button class="btn btn-primary" type="submit">Genera</button>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 text-center">
+        <?php if($int_pass_length > 0) { ?>
+          <p class="text-break"><?= getRandomPassword($pass_length) ?></p>
+        <?php } else { ?>
+          <p>Inserire numero maggiore di 0 !</p>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
