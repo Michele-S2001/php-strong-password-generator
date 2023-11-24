8ff46ee47d71907b67866789449a3328dd8d3b9a
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-24 10:08:03 +0100

    REFACTOR > Func of password generator

--- functions.php
+++ functions.php
@@ -3,18 +3,24 @@
 function getRandomPassword($passLength) {
   $random_pass = "";
 
-  while(strlen($random_pass) < $passLength) {
-    $upper_case_letter = chr(rand(65, 90));
-    $lower_case_letter = chr(rand(97, 122));
-    $random_symbol = chr(rand(33, 38));
-    $random_num = rand(1, 9);
-    $random_pass .= $upper_case_letter;
-    $random_pass .= $random_num;
-    $random_pass .= $lower_case_letter;
-    $random_pass .= $random_symbol;
+  for($i = 0; $i < $passLength; $i++) {
+    $rand_char = chr(rand(33, 158));
+    $random_pass .= $rand_char;
   }
   
-  $final_pass = str_shuffle(substr($random_pass, 0, $passLength));
-
-  return $final_pass;
+  return $random_pass;
 }
+
+// while(strlen($random_pass) < $passLength) {
+//   // $upper_case_letter = chr(rand(65, 90));
+//   // $lower_case_letter = chr(rand(97, 122));
+//   // $random_symbol = chr(rand(33, 38));
+//   // $random_num = rand(1, 9);
+//   // $random_pass .= $upper_case_letter;
+//   // $random_pass .= $random_num;
+//   // $random_pass .= $lower_case_letter;
+//   // $random_pass .= $random_symbol;
+// }
+
+// $final_pass = str_shuffle(substr($random_pass, 0, $passLength));
+// return $final_pass;
